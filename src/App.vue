598fef7771e39a339c5b8e0b4ba70bc8d075d4c2
joe <zdnf_lgq@163.com>
2020-08-02 15:42:46 +0800

    date-coms

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <date-com/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import dateCom from '@/components/date'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    dateCom
   }
 }
 </script>
---
+++ src/components/date.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        时间组件
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'dateCom',
+        updated () {
+        },
+        mounted () {
+            console.log(this.dealDate({year:2015,max:11,min:'',split:'-',reverse:true}));
+        },
+        methods: {
+            dealDate({year=new Date().getFullYear(),min='',max='',split='/',reverse=false}) {
+                /* 
+                min:开始月份(返回值包含)
+                max:结束月份（返回值包含）
+                */
+               let dateArr = []
+                if(min && max){
+                    dateArr =  [...new Array(max-min+1)].map((item,idx)=>{
+                        if(idx<=max){
+                            return `${year}${split}${max-min+idx-1}`
+                        }
+                    },[])
+                    return reverse ? dateArr.reverse() : dateArr
+                }
+                if(min){
+                    dateArr = [...new Array(12-min+1)].map((item,idx)=>{
+                        if(idx<=12){
+                            return `${year}${split}${min+idx}`
+                        }
+                    },[])
+                    return reverse ? dateArr.reverse() : dateArr
+                }
+                if(max){
+                    dateArr = [...new Array(max)].map((item,idx)=>{
+                        if(idx<max){
+                            return `${year}${split}${idx+1}`
+                        }
+                    },[])
+                    return reverse ? dateArr.reverse() : dateArr
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
